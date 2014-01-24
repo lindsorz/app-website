626383c11faeb77d0ea49ed724917a73c58540d9
Lindsorz <Lindsorz@Lindsays-MacBook-Pro-2.local>
2014-01-23 21:09:49 -0500

    mega commit

--- js/main.js
+++ js/main.js
@@ -33,3 +33,27 @@ $('.previous').on('click', function() {
 	
 	switchItem(current, previous);
 });
+
+var $videoDialog = $('dialog');
+dialogPolyfill.registerDialog($videoDialog.get(0));
+
+$('.btn-open').on('click', function () {
+	$videoDialog.children('.video').html('<iframe src="http://player.vimeo.com/video/81447092?color=c9ff23&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+	/* When triggering built-in JS functions we need to bypass jQuery
+	In order to get to the raw JS element we cas use .get()
+	
+	Example:
+	$videoDialog.show() - will trigger jQuery's show function
+	$videoDialog.get(0).show - will get the first raw JS element and trigger its native show function
+	*/
+	$videoDialog.get(0).showModal();
+	/*
+	.show() allowes uers to interact with stuff behind the dialog
+	.showModal() disables user interaction behind the dialog
+	*/
+});
+
+$('.btn-close').on('click', function (){
+	$videoDialog.get(0).close();
+	$videoDialog.children('.video').html('');
+});
